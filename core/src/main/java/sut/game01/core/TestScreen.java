bdd47ef50ae6bcbe105d594f264c8271c0921ddf
JutamasGomol <Jutamas.GM@gmail.com>
2016-03-23 15:59:35 +0700

    Add new

--- core/src/main/java/sut/game01/core/TestScreen.java
+++ core/src/main/java/sut/game01/core/TestScreen.java
@@ -22,6 +22,8 @@ public class TestScreen extends Screen {
    // private final Image bgImage; 
     private final ImageLayer backButton;
    // private final Image backImage;
+    private final ImageLayer startButton;
+    private final ImageLayer exitButton;
 
   public TestScreen(final ScreenStack ss) {
     this.ss = ss;
@@ -29,7 +31,7 @@ public class TestScreen extends Screen {
     Image bgImage = assets().getImage("images/bg.png");
     this.bg = graphics().createImageLayer(bgImage);
 
-    Image backImage = assets().getImage("images/backbutton.png");
+    Image backImage = assets().getImage("images/menu.png");
     this.backButton = graphics().createImageLayer(backImage);
     backButton.setTranslation(10, 10);
     
@@ -39,6 +41,31 @@ public class TestScreen extends Screen {
             ss.remove(ss.top()); // pop screen
           }
       });
+
+    Image startImage = assets().getImage("images/start.png");
+    this.startButton = graphics().createImageLayer(startImage);
+    startButton.setTranslation(100, 100);
+    
+    startButton.addListener(new Mouse.LayerAdapter(){
+          @Override
+          public void onMouseUp(Mouse.ButtonEvent event){
+            ss.remove(ss.top()); // pop screen
+          }
+      });
+
+    Image exitImage = assets().getImage("images/exit.png");
+    this.exitButton = graphics().createImageLayer(exitImage);
+    exitButton.setTranslation(200, 200);
+    
+    exitButton.addListener(new Mouse.LayerAdapter(){
+          @Override
+          public void onMouseUp(Mouse.ButtonEvent event){
+            ss.remove(ss.top()); // pop screen
+          }
+      });
+
+
+
    }
   
 
@@ -47,6 +74,8 @@ public class TestScreen extends Screen {
       super.wasShown();
       this.layer.add(bg);
       this.layer.add(backButton);
+      this.layer.add(startButton);
+      this.layer.add(exitButton);
 
     }
 }
